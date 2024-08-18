be438df972300f940499373b54ffe4fb3888aaf6
PC <mixail.ermin33@gmail.com>
2024-08-18 12:04:00 +0300

    test

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app</Text>
+      <Text style={styles.mainText}>Product Scanner app</Text>
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +18,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  mainText: {
+    color: "red",
+    fontSize: 20
+  }
 });
